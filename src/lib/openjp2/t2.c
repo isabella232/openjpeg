d43fae38e1b06efe3ecd6e7bdf8a562e0f3ea237
Jean-Francois Pambrun <jf.pambrun@gmail.com>
2016-04-11 10:15:09 -0400

    Relaxed conditions to support truncated streams.
    May require further testing for memory leaks.

--- src/lib/openjp2/t2.c
+++ src/lib/openjp2/t2.c
@@ -1157,9 +1157,9 @@ static OPJ_BOOL opj_t2_read_packet_data(   opj_t2_t* p_t2,
                         do {
                                 /* Check possible overflow (on l_current_data only, assumes input args already checked) then size */
                                 if ((((OPJ_SIZE_T)l_current_data + (OPJ_SIZE_T)l_seg->newlen) < (OPJ_SIZE_T)l_current_data) || (l_current_data + l_seg->newlen > p_src_data + p_max_length)) {
-                                        opj_event_msg(p_manager, EVT_ERROR, "read: segment too long (%d) with max (%d) for codeblock %d (p=%d, b=%d, r=%d, c=%d)\n",
+                                        opj_event_msg(p_manager, EVT_WARNING, "read: segment too long (%d) with max (%d) for codeblock %d (p=%d, b=%d, r=%d, c=%d)\n",
 																								l_seg->newlen, p_max_length, cblkno, p_pi->precno, bandno, p_pi->resno, p_pi->compno);
-                                        return OPJ_FALSE;
+                                        return OPJ_TRUE;
                                 }
 
 #ifdef USE_JPWL
