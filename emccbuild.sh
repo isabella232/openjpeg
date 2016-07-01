0d3dc16453a359f75fa9d47af520bcc1af6babca
chafey <chafey@gmail.com>
2016-07-01 10:23:06 -0700

    added node build renamed bowser build added example to show use from node

--- emccbuild.sh
+++ emccbuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir build
 emcc bin/libopenjpeg-js.bc -o \
-     build/openJPEG-FixedMemory.js \
+     build/openJPEG-FixedMemory-browser.js \
      --memory-init-file 0 \
      -s EXPORTED_FUNCTIONS="['_jp2_decode']" \
      -s TOTAL_MEMORY=400000000 \
@@ -10,11 +10,27 @@ emcc bin/libopenjpeg-js.bc -o \
      -s MODULARIZE=1 \
      -O3
 emcc bin/libopenjpeg-js.bc -o \
-     build/openJPEG-DynamicMemory.js \
+     build/openJPEG-DynamicMemory-browser.js \
      --memory-init-file 0 \
      -s EXPORTED_FUNCTIONS="['_jp2_decode']" \
      -s ALLOW_MEMORY_GROWTH=1 \
      -s NO_FILESYSTEM=1 \
      -s EXPORT_NAME="'OpenJPEG'" \
      -s MODULARIZE=1 \
+     -O3
+emcc bin/libopenjpeg-js.bc \
+     -o build/openJPEG-FixedMemory-commonJS.js \
+     --memory-init-file 0 \
+     -s EXPORTED_FUNCTIONS="['_jp2_decode']" \
+     -s TOTAL_MEMORY=400000000 \
+     -s NO_FILESYSTEM=1 \
+     -s DISABLE_EXCEPTION_CATCHING=0 \
+     -O3
+emcc bin/libopenjpeg-js.bc \
+     -o build/openJPEG-DynamicMemory-commonJS.js \
+     --memory-init-file 0 \
+     -s EXPORTED_FUNCTIONS="['_jp2_decode']" \
+     -s ALLOW_MEMORY_GROWTH=1 \
+     -s NO_FILESYSTEM=1 \
+     -s DISABLE_EXCEPTION_CATCHING=0 \
      -O3
